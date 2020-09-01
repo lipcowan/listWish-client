3253b897b9601f213d9590b20b698ba2e0bc1e70
Lip Cowan <pcowan90@gmail.com>
2020-09-01 15:03:17 -0700

    ready for deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
-    TOKEN_KEY: 'listwish-frontend-auth-token'
+    API_ENDPOINT: 'https://obscure-caverns-86237.herokuapp.com/api',
+    TOKEN_KEY: 'listwish-frontend-auth-token',
+    API_KEY: process.env.REACT_APP_API_KEY
 }
